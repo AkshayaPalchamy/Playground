1e8696a6515c438a9a87bdefd9c27cb576918e5c
Akshaya <akshaya@astravor.com>
2026-03-23 16:08:00 +0530

    ADDED: Aspire Hosting template and Products controller

---
+++ src/apps/Playground.Api/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Playground.Api.Models;
+
+namespace Playground.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly List<Product> Products = new()
+        {
+            new Product {ProductId = 1,ProductName = "book",ProductCategory="Study",ProductDescription="Finance Book"}
+        };
+
+        //GET
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(Products);
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult AddProducts(int id, [FromBody] Product products)
+        {
+            Products.Add(products);
+            return CreatedAtAction(nameof(GetProducts), new { id = products.ProductId }, products);
+        }
+
+        //PUT
+        [HttpPut("{id}")]
+        public IActionResult UpdateProducts(int id, [FromBody]Product product)
+        {
+            Product? Product = Products.FirstOrDefault(p => p.ProductId == id);
+            if (Products == null) return NotFound();
+            Product.ProductName = product.ProductName;
+            Product.ProductId = product.ProductId;
+            Product.ProductDescription = product.ProductDescription;
+
+            return Ok(product);
+        }
+
+        //DELETE
+        public IActionResult DeleteProducts(int id)
+        {
+            Product products = Products.FirstOrDefault(b => b.ProductId == id);
+            if (Products == null) return NotFound();
+            Products.Remove(products);
+            return NoContent();
+        }
+    }
+}
---
+++ src/apps/Playground.Api/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Playground.Api.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductCategory { get; set; }
+    }
+}
