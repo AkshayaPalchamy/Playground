538da1ca771c9a514f945e2f34db2e53de8bede2
Akshaya <akshaya@astravor.com>
2026-03-24 19:09:24 +0530

    ADDED: UI-Design

--- PlayGround.Host/AppHost.cs
+++ PlayGround.Host/AppHost.cs
@@ -4,4 +4,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Playground_Api>("PlayGround");
 
+builder.AddProject<Playground_web>("playground-web");
+
 builder.Build().Run();
